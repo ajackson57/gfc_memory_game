67bde4ef0a13a368e44e063dd78fc84763a81ef5
n0297270 <andrewm.jackson@libertymutual.com>
2017-09-05 11:17:37 -0400

    Add array of objects to store cards.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
 // var cardOne = cards[0];
 // var cardTwo = cards[1];
@@ -16,8 +38,10 @@ var checkForMatch = function(){
 }
 var flipCard = function(cardId){
 
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 
